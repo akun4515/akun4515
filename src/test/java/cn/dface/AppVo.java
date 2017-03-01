e8879fb1fdf488d5a487927fd32908225b04b8e5
yk.wu <yk.wu@dface.cn>
2017-03-01 12:58:18 +0800

    u

---
+++ src/test/java/cn/dface/AppVo.java
@@ -0,0 +1,25 @@
+/**
+ * Project: myApp
+ * 
+ * File Created at 2017年3月1日
+ * $Id$
+ * 
+ * Copyright 2015 dface.cn Croporation Limited.
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information of
+ * dface Company. ("Confidential Information").  You shall not
+ * disclose such Confidential Information and shall use it only in
+ * accordance with the terms of the license agreement you entered into
+ * with www.dface.cn
+ */
+package cn.dface;
+
+/**
+ * TODO Comment of AppVo
+ * @author akun
+ *
+ */
+public class AppVo {
+
+}
